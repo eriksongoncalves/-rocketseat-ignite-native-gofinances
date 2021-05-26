c3f08881acd2c514bfe4a863dc0dfd2f640b0187
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-05-26 11:23:32 -0300

    fix: schema

--- src/screens/Register/schema.ts
+++ src/screens/Register/schema.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 export const schema = Yup.object().shape({
   name: Yup.string().required('O nome é obrigatório'),
-  price: Yup.number()
+  amount: Yup.number()
     .typeError('Informe um valor númerico')
     .positive('O valor não pode ser negativo')
     .required('O preço é obrigatório')
